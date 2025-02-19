e65ce0678aca8a9dc14298ae24bb1486c1eac832
Muhammad Aji Massaid <ajimassaid.jobs@gmail.com>
2025-02-20 06:05:31 +0700

    change default host url

--- fe-next/src/redux/slices/menuSlice.ts
+++ fe-next/src/redux/slices/menuSlice.ts
@@ -20,7 +20,7 @@ interface MenuState {
   loading: boolean;
   error: string | null;
 }
-const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || "http://localhost:4019";
+const API_BASE_URL = process.env.REACT_APP_API_BASE_URL||'https://mono-repo-menu-management-production.up.railway.app/';
 
 export const fetchMenuHierarchy = createAsyncThunk("menu/fetchHierarchy", async () => {
   const response = await axios.get(`${API_BASE_URL}/menus/hierarchy`);
